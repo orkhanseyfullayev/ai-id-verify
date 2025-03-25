f2b37a5e275e98c0737bad1727dadd7ba3537256
Orkhan Seyfullayev <orkhan.seyfullayev@gmail.com>
2025-03-25 22:03:52 +0300

    Completed text_detection.py without integreted another.

--- text_detection.py
+++ text_detection.py
@@ -67,6 +67,3 @@ os.makedirs(result_folder, exist_ok=True)
 file_utils.saveResult(image_path, image[:, :, ::-1], boxes, dirname=result_folder)
 
 print("✅ Metin tespiti tamamlandı. Sonuç ./result klasörüne kaydedildi.")
-
-
-# test
